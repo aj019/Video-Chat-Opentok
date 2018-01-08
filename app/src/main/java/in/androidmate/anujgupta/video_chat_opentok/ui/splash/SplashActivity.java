a03902cbeb460cd278b9a582952170b8898b9617
Anuj Gupta <junagupta19@gmail.com>
2018-01-08 12:12:34 +0530

    Created required packages and renamed MainActivity to SplashActivity

---
+++ app/src/main/java/in/androidmate/anujgupta/video_chat_opentok/ui/splash/SplashActivity.java
@@ -0,0 +1,15 @@
+package in.androidmate.anujgupta.video_chat_opentok.ui.splash;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import in.androidmate.anujgupta.video_chat_opentok.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
